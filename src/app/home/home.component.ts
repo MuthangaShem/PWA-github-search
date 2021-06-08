a3a9c15692e0eacfeb1ee1adc89f88cb44398f56
muthangashem <muthangashem@gmail.com>
2021-06-08 21:12:04 +0300

    search with debounced observable string event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,10 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  search(event: Observable<string>) {
+    event.subscribe(search => console.log('search', search));
   }
 
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -9,7 +11,26 @@ export class SearchComponent implements OnInit {
 
   constructor() { }
 
+  @Output() results = new EventEmitter<Observable<string>>();
+
+  @ViewChild('input', { static: true }) input!: ElementRef;
+
   ngOnInit() {
+    this.searchMethod();
+  }
+
+  ngAfterViewInit() {
+  }
+
+  searchMethod() {
+    this.results.emit(
+      fromEvent(this.input.nativeElement, 'keyup')
+        .pipe(
+          debounceTime(500),
+          map(searchValue => this.input.nativeElement.value.trim()),
+          distinctUntilChanged()
+        )
+    );
   }
 
 }
